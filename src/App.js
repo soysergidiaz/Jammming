035213bc35066935108ed600b93cfe0a22dc34b9
Sergi Díaz <sdiazolivas@gmail.com>
2024-04-13 18:49:00 +0200

    Searchbar added + Styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar.jsx';
+import './components/comps.css';
 
 function App() {
   return (
-    <header>
-      <h1>Ja<span>mmm</span>ing</h1>
-    </header>
+    <>
+      <header>
+        <h1>Ja<span>mmm</span>ing</h1>
+      </header>
+
+      <div className="search-bar">
+        <SearchBar />
+      </div>
+    </>
   );
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function SearchBar() {
   return (
     <div>
+
+        <h2>Busca una canción</h2>
+        <input type="text" id='songs-finder' />
       
     </div>
   )
 }
 
-export default SearchBar
+export default SearchBar;
