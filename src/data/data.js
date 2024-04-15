b962f9d173bfb22781985c7c010439fd49115e64
Sergi Díaz <161464330+soysergidiaz@users.noreply.github.com>
2024-04-15 19:10:30 +0200

    Created data test

---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+const songs = [{
+        title: "Lonely",
+        author: "Justin Bieber",
+        album: "Justice",
+        id: 1
+    },
+    {
+        title: "Manifiesto",
+        author: "Nach",
+        album: "Un día en suburbia",
+        id: 2
+    }
+];
+
+export default songs;
